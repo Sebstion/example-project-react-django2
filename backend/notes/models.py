d8de0cae47492c934eba2da91f1c4a1045944224
dominik.doberski <dominik.doberski@wundermanthompson.com>
2023-11-03 16:34:20 +0100

    Add user to database

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.username
+
 class Note(models.Model):
     note_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__():
+    def __str__(self):
         return self.note_text
