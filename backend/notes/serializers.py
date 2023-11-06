1fe421de30d03de5f9b1a371cf85bc8fc87f2a84
dominik.doberski <dominik.doberski@wundermanthompson.com>
2023-11-06 11:07:52 +0100

    Add owner serializer

--- backend/notes/serializers.py
+++ backend/notes/serializers.py
@@ -5,4 +5,10 @@ class NoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Note
-        fields = ("pk", "note_text", "pub_date")
+        fields = ("pk", "note_text", "pub_date", "owner")
+
+class OwnerSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = Note
+            fields = ("pk", "username", "password")
